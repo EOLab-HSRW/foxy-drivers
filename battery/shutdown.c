4d5a16d6afe0e764b0dd8b64328f040bb233c121
harleylara <harley.lara@outlook.com>
2026-05-22 10:56:22 +0200

    add shutdown demo

---
+++ battery/shutdown.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define BATTERYD_IMPLEMENTATION
+#include "battery.h"
+
+int main(void) {
+    batteryd_status s;
+
+    int rc = batteryd_get(&s); /* reads /run/batteryd/status */
+    if (rc != BATTERYD_OK) {
+        fprintf(stderr, "batteryd: %s\n", batteryd_strerror(rc));
+        return 1;
+    }
+
+    if (s.online) {
+        printf("battery: %d%%, %dmV, %dmA\n",
+               s.percent, s.voltage_mv, s.current_ma);
+    } else {
+        printf("battery offline");
+        if (s.error[0]) printf(": %s", s.error);
+        printf("\n");
+    }
+
+    batteryd_shutdown(NULL);        /* sends SHUTDOWN */
+
+    return 0;
+}
