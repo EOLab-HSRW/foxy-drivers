2aa92fa7d2f86ccca6b9f94d038a97bfe6e11c30
harleylara <harley.lara@outlook.com>
2026-06-10 15:47:59 +0200

    relocate header

--- usr/local/include/foxy/battery.h
+++
@@ -1,7 +0,0 @@
-#ifndef FOXY_BATTERY_H_INCLUDED
-#define FOXY_BATTERY_H_INCLUDED
-
-/* Compatibility include. New code should include <foxy/batteryd.h>. */
-#include "batteryd.h"
-
-#endif /* FOXY_BATTERY_H_INCLUDED */
